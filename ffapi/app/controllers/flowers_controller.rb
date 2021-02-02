2ded1213e3098acad70e392645e6c8a758fab5c0
mavrik83 <rjhuber83@gmail.com>
2021-02-02 11:22:26 -0500

    add show action to flower controller

--- ffapi/app/controllers/flowers_controller.rb
+++ ffapi/app/controllers/flowers_controller.rb
@@ -5,4 +5,15 @@ class FlowersController < ApplicationController
     render json: FlowerSerializer.new(flowers)
   end
 
+  def show
+    flower = Flower.find_by(id: params[:id])
+
+    render json: FlowerSerializer.new(flower)
+  end
+
+  private
+
+  def flower_params
+    params.require(:flower).permit(:id)
+  end
 end
