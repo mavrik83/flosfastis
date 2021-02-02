5a0b81e639ab5057d2ef99f19fb9f97491b02c60
mavrik83 <rjhuber83@gmail.com>
2021-02-02 11:26:55 -0500

    add selectFlower redux action

--- ffclient/src/actions/index.js
+++ ffclient/src/actions/index.js
@@ -7,3 +7,11 @@ export const fetchFlowers = () => async (dispatch) => {
   dispatch({ type: "FETCH_FLOWERS", payload: response.data });
 };
 
+export const selectFlower = (id) => async (dispatch) => {
+  const response = await fetch(`http://localhost:3002/flowers/${id}`)
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+  dispatch({ type: "SELECT_FLOWER", payload: response.data });
+};
