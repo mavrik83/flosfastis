0405859df7b63edb84d8b64904ebcfbbfd3b5568
mavrik83 <rjhuber83@gmail.com>
2021-02-02 11:31:29 -0500

    update dependencies in index.js

--- ffclient/src/index.js
+++ ffclient/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import "semantic-ui-css/semantic.min.css";
+
+import App from "./App";
+import reducers from "./reducers";
 
 ReactDOM.render(
   <React.StrictMode>
