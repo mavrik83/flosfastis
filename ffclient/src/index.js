afacfc905cc93ab098f46639dc35f4f4a10429cc
mavrik83 <rjhuber83@gmail.com>
2021-02-02 11:31:51 -0500

    add redux devtools and store to index.js

--- ffclient/src/index.js
+++ ffclient/src/index.js
@@ -8,14 +8,12 @@ import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import reducers from "./reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
