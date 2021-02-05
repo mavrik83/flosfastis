5e68ec403aca10057c458348a32ec9474c7f3ee8
mavrik83 <rjhuber83@gmail.com>
2021-02-04 22:57:34 -0500

    add route for the scrape controller

--- ffapi/config/routes.rb
+++ ffapi/config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   resources :flowers
   resources :schedules
+  get '/scrape/:zipCode' => 'scrape#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
