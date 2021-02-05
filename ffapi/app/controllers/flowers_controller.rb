74beb2018b379aba08e4a0ee680d202ead08fd42
mavrik83 <rjhuber83@gmail.com>
2021-02-04 22:55:26 -0500

    add create action in flowers controller

--- ffapi/app/controllers/flowers_controller.rb
+++ ffapi/app/controllers/flowers_controller.rb
@@ -8,12 +8,16 @@ class FlowersController < ApplicationController
   def show
     flower = Flower.find_by(id: params[:id])
 
+  def create
+    flower = Flower.new(flower_params)
+    flower.user_created = true
+    flower.save
     render json: FlowerSerializer.new(flower)
   end
 
   private
 
   def flower_params
-    params.require(:flower).permit(:id)
+    params.require(:flower).permit(:id, :variety, :sowing_time, :soil_temp, :planting_depth, :grow_height, :spread, :sun_light, :annual, :sow_indoors, :transplant, :days_to_maturity, :user_created)
   end
 end
