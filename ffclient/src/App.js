4b624efa87bcd60c69a2275d46178287c02591d3
mavrik83 <rjhuber83@gmail.com>
2021-02-04 23:03:05 -0500

    Update App.js

--- ffclient/src/App.js
+++ ffclient/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchFlowers } from "./actions";
 import FlowerDetail from "./components/FlowerDetail";
 import FlowerList from "./components/FlowerList";
+import FlowerCreate from "./components/FlowerCreate";
 import Header from "./components/Header";
+// import Timeline from "./components/Timeline";
 
 class App extends Component {
+  componentDidMount() {
+    localStorage.clear();
+    this.props.fetchFlowers();
+  }
   render() {
     return (
-      <div className="ui container transparent">
+      <div className="ui container">
         <BrowserRouter>
           <Header />
-          <Route path="/" exact component={FlowerList} />
-          <Route path="/flowers/:id" exact component={FlowerDetail} />
+          {/* <Timeline /> */}
+          <Switch>
+            <Route path="/" exact component={FlowerList} />
+            <Route path="/flowers/new" exact component={FlowerCreate} />
+            <Route path="/flowers/:id" exact component={FlowerDetail} />
+          </Switch>
         </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, { fetchFlowers: fetchFlowers })(App);
