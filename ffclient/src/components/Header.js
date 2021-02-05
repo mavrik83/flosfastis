f5a3b7f59563e18162a6ad7c382b857c7841ed98
mavrik83 <rjhuber83@gmail.com>
2021-02-04 23:05:39 -0500

    adds frost date info to header bar

--- ffclient/src/components/Header.js
+++ ffclient/src/components/Header.js
@@ -1,39 +1,16 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
+import FrostInfo from "./FrostInfo";
 import { NavLink } from "react-router-dom";
 
 class Header extends Component {
-  state = { activeItem: "home" };
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
-
   render() {
-    const { activeItem } = this.state;
-
     return (
       <div>
         <Menu pointing secondary style={{ marginTop: "10px" }}>
-          <Menu.Item
-            as={NavLink}
-            to="/"
-            content="Flos Fastis"
-            name="home"
-            active={activeItem === "home"}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            as={NavLink}
-            to="/flowers/show"
-            name="messages"
-            active={activeItem === "messages"}
-            onClick={this.handleItemClick}
-          />
+          <Menu.Item as={NavLink} to="/" content="Flos Fastis" name="home" />
           <Menu.Menu position="right">
-            <Menu.Item
-              name="logout"
-              active={activeItem === "logout"}
-              onClick={this.handleItemClick}
-            />
+            <FrostInfo />
           </Menu.Menu>
         </Menu>
       </div>
