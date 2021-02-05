862faf46b65f55b5b02798532ccafebed2ec1f45
mavrik83 <rjhuber83@gmail.com>
2021-02-04 23:07:08 -0500

    add local storage to persist redux state through browser refresh

--- ffclient/src/index.js
+++ ffclient/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import "semantic-ui-css/semantic.min.css";
+import { throttle } from "lodash";
+import { loadState, saveState } from "./utils/localStorage";
 
 import App from "./App";
 import reducers from "./reducers";
 
+const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  reducers,
+  persistedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
 
 ReactDOM.render(
   <Provider store={store}>
