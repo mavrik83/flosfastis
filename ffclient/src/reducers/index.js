6b2890bf2ab3e201be904ea7e10e5f2998ea26fa
mavrik83 <rjhuber83@gmail.com>
2021-02-04 23:07:53 -0500

    wire in redux form

--- ffclient/src/reducers/index.js
+++ ffclient/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
 import flowersReducer from "./flowersReducer";
 import selectedReducer from "./selectFlower";
 
 export default combineReducers({
   flowers: flowersReducer,
   selectedFlower: selectedReducer,
+  form: formReducer,
 });
