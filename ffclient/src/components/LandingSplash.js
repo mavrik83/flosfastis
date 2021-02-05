50c3d6469adbc5687c65e2a3e0ce960c4c011ae3
mavrik83 <rjhuber83@gmail.com>
2021-02-04 23:06:05 -0500

    adds zip code input field to home page

--- ffclient/src/components/LandingSplash.js
+++ ffclient/src/components/LandingSplash.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Segment, Button, Header } from "semantic-ui-react";
-// import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import ZipCodeSubmit from "./ZipCodeSubmit";
 
 const LandingSplash = () => {
   return (
@@ -24,6 +25,8 @@ const LandingSplash = () => {
               Welcome! To begin planning for your flower planting season, select
               a flower from the list.
             </p>
+
+            <ZipCodeSubmit />
           </Segment>
         </Grid.Row>
         <Grid.Row>
@@ -37,7 +40,9 @@ const LandingSplash = () => {
               If there is a flower that is not listed, you may add it to the
               list.
             </p>
-            <Button>Add New Flower</Button>
+            <Link to={"/flowers/new"}>
+              <Button>Add New Flower</Button>
+            </Link>
           </Segment>
         </Grid.Row>
       </Grid.Column>
