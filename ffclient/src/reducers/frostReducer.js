416805ec4b7b15f441f59fba1bd14ed3585eef7a
mavrik83 <rjhuber83@gmail.com>
2021-02-06 00:26:15 -0500

    Update frostReducer.js

--- ffclient/src/reducers/frostReducer.js
+++ ffclient/src/reducers/frostReducer.js
@@ -2,6 +2,8 @@ const frostReducer = (state = [], action) => {
   switch (action.type) {
     case "GET_FROST":
       return action.payload;
+    case "CLEAR_FROST":
+      return action.payload;
     default:
       return state;
   }
