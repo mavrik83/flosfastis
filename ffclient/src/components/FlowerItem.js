8497f8b7b112de55c6d002da10e7ae5a16937242
mavrik83 <rjhuber83@gmail.com>
2021-02-06 00:25:44 -0500

    Update FlowerItem.js

--- ffclient/src/components/FlowerItem.js
+++ ffclient/src/components/FlowerItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Segment, Button, Header } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
-import ButtonDelete from "./ButtonDelete";
+import FlowerDelete from "./FlowerDelete";
 
 const FlowerItem = (props) => {
   return (
@@ -9,7 +9,8 @@ const FlowerItem = (props) => {
       <Button floated="right" compact as={NavLink} to={`/flowers/${props.id}`}>
         View
       </Button>
-      <ButtonDelete condition={props.condition} id={props.id} />
+
+      <FlowerDelete condition={props.condition} id={props.id} />
       <Header floated="left">{props.name}</Header>
     </Segment>
   );
