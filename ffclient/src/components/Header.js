e8df3d75171898b283adda3a5a67da7dd4af14d3
mavrik83 <rjhuber83@gmail.com>
2021-02-06 00:26:00 -0500

    Update Header.js

--- ffclient/src/components/Header.js
+++ ffclient/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 import FrostInfo from "./FrostInfo";
 import { NavLink } from "react-router-dom";
+import CreateButton from "./CreateButton";
 
 class Header extends Component {
   render() {
@@ -9,6 +10,7 @@ class Header extends Component {
       <div>
         <Menu pointing secondary style={{ marginTop: "10px" }}>
           <Menu.Item as={NavLink} to="/" content="Flos Fastis" name="home" />
+          <CreateButton />
           <Menu.Menu position="right">
             <FrostInfo />
           </Menu.Menu>
