7466d66fb08491fb14e1d36b0cbc8b64445b1f7a
mavrik83 <rjhuber83@gmail.com>
2021-02-06 00:26:19 -0500

    Update index.js

--- ffclient/src/reducers/index.js
+++ ffclient/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import flowersReducer from "./flowersReducer";
 import frostReducer from "./frostReducer";
+import eventReducer from "./eventReducer";
 
 export default combineReducers({
   flowers: flowersReducer,
   form: formReducer,
   frost: frostReducer,
+  events: eventReducer,
 });
