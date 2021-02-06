623505419145308f9b36528fadab42af580584a3
mavrik83 <rjhuber83@gmail.com>
2021-02-06 11:13:30 -0500

    Create FlowerDelete.js

---
+++ ffclient/src/components/flowers/FlowerDelete.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { deleteFlower } from "../../actions";
+
+class FlowerDelete extends Component {
+  onClick = () => {
+    this.props.deleteFlower(this.props.id);
+  };
+
+  render() {
+    if (!this.props.condition) {
+      return <></>;
+    }
+    return (
+      <>
+        <Button color="red" compact floated="right" onClick={this.onClick}>
+          Delete
+        </Button>
+      </>
+    );
+  }
+}
+
+export default connect(null, { deleteFlower })(FlowerDelete);
