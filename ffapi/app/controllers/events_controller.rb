a26d9da74498b45b7b3783a0e8400c561eb8e013
mavrik83 <rjhuber83@gmail.com>
2021-02-06 00:17:15 -0500

    update events controller for delete method

--- ffapi/app/controllers/events_controller.rb
+++ ffapi/app/controllers/events_controller.rb
@@ -11,3 +11,15 @@ class EventsController < ApplicationController
     event.save
     render json: EventSerializer.new(event)
   end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.delete
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :begin_time, :harvest_day, :planting_day)
+  end
+end
