b6f276bb5e3e1f8c72adfa8fc5411c4f088c574f
mavrik83 <rjhuber83@gmail.com>
2021-02-06 11:13:25 -0500

    add validations to form

---
+++ ffclient/src/components/flowers/FlowerCreate.js
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { Grid, Segment, Header } from "semantic-ui-react";
+import { createFlower } from "../../actions";
+import {
+  required,
+  sowingFormat,
+  daysToMatFormat,
+} from "../../utils/validations";
+
+class FlowerCreate extends Component {
+  renderInput = ({
+    input,
+    label,
+    placeholder,
+    className,
+    meta: { touched, error, warning },
+  }) => {
+    return (
+      <div className={`${className} field`}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" placeholder={placeholder} />
+        {touched &&
+          ((error && (
+            <span className="ui pointing red basic label">{error}</span>
+          )) ||
+            (warning && (
+              <span className="ui pointing yellow basic label">{warning}</span>
+            )))}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.createFlower(formValues);
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <Grid style={{ marginTop: "15px" }}>
+        <Grid.Column width="3"></Grid.Column>
+        <Grid.Column width="8">
+          <Segment basic textAlign="center">
+            <Header>Add New Flower</Header>
+            <p>
+              Enter the following information. Leave a field blank if you do not
+              know the information.
+            </p>
+          </Segment>
+          <form
+            className="ui form small"
+            onSubmit={this.props.handleSubmit(this.onSubmit)}
+          >
+            <div className="inline fields">
+              <label>
+                <Field
+                  name="annual"
+                  component="input"
+                  type="radio"
+                  value="true"
+                />{" "}
+                Annual
+              </label>
+              <label>
+                <Field
+                  name="annual"
+                  component="input"
+                  type="radio"
+                  value="false"
+                />{" "}
+                Perennial
+              </label>
+            </div>
+            <div className="inline fields">
+              <label>
+                <Field name="sow_indoors" component="input" type="checkbox" />{" "}
+                Should Plant be Sowed Inside?
+              </label>
+            </div>
+            <Field
+              name="variety"
+              label="Variety"
+              component={this.renderInput}
+              placeholder="Enter the name of the flower"
+              validate={required}
+              className="required field"
+            />
+            <Field
+              name="sowing_time"
+              label="Indoor Sowing Range"
+              component={this.renderInput}
+              placeholder="How many weeks before or after first frost?"
+              validate={[required, sowingFormat]}
+              className="required field"
+            />
+
+            <Field
+              name="grow_height"
+              label="Grow Height"
+              component={this.renderInput}
+              placeholder="Enter the growth height in inches..."
+            />
+            <Field
+              name="spread"
+              label="Planting Spread"
+              component={this.renderInput}
+              placeholder="Enter the planting spread in inches..."
+            />
+            <Field
+              name="soil_temp"
+              label="Ideal Soil Temperature"
+              component={this.renderInput}
+              placeholder="Enter the ideal soil temperature in &deg;F..."
+            />
+            <Field
+              name="sun_light"
+              label="Required Sun Light"
+              component={this.renderInput}
+              placeholder="Enter the sunlight requirements..."
+            />
+            <Field
+              name="days_to_maturity"
+              label="Days to Maturity"
+              component={this.renderInput}
+              placeholder="How many days unitl harvest?"
+              validate={[required, daysToMatFormat]}
+              className="required field"
+            />
+            <Field
+              name="planting_depth"
+              label="Planting Depth"
+              component={this.renderInput}
+              placeholder="How many inches deep should seeds be sowed?"
+            />
+            <Field
+              name="transplant"
+              label="Transplant Time"
+              component={this.renderInput}
+              placeholder="When should plant be transplanted?"
+            />
+            <button className="ui button">Submit</button>
+          </form>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+const formWrapped = reduxForm({
+  form: "flowerCreate",
+})(FlowerCreate);
+
+export default connect(null, { createFlower })(formWrapped);
---
+++ ffclient/src/utils/validations.js
@@ -0,0 +1,15 @@
+export const required = (value) => {
+  return value ? undefined : "Required";
+};
+
+export const sowingFormat = (value) => {
+  return value && !/^[0-9]{1,3}[-]{1}[0-9]{1,3} [a-z]{5} [a-z]{6}$/.test(value)
+    ? `Invalid format. Ex: "55-65 weeks before".`
+    : undefined;
+};
+
+export const daysToMatFormat = (value) => {
+  return value && !/^[0-9]{1,3}[-]{1}[0-9]{1,3}$/.test(value)
+    ? `Invalid format. Ex: "55-65"`
+    : undefined;
+};
