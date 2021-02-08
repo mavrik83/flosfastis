b3c21594505d2d8b7cc2d450245d5335136b075b
mavrik83 <rjhuber83@gmail.com>
2021-02-07 22:36:32 -0500

    add error handling to ScrapeController

--- ffapi/app/controllers/scrape_controller.rb
+++ ffapi/app/controllers/scrape_controller.rb
@@ -3,13 +3,21 @@ class ScrapeController < ApplicationController
     slug = params[:zipCode]
     url = URI.open("https://www.almanac.com/gardening/frostdates/zipcode/#{slug}")
     doc = Nokogiri::HTML(url)
-    loc = doc.css('td')[0].text
-    alt = doc.css('td')[1].text
-    last_spring_frost = doc.css('td')[2].text
-    first_fall_frost = doc.css('td')[3].text
-    growing_season = doc.css('td')[4].text
+    if doc && doc.css('th')[0]
+      loc = doc.css('td')[0].text
+      alt = doc.css('td')[1].text
+      last_spring_frost = doc.css('td')[2].text
+      first_fall_frost = doc.css('td')[3].text
+      growing_season = doc.css('td')[4].text
+    else
+      loc = 'Data unavailable, default provided.'
+      alt = ''
+      last_spring_frost = 'Apr 15'
+      first_fall_frost = ''
+      growing_season = ''
+    end
 
-    frost_data = {"location": loc, "altitude": alt, "lastSpringFrost": last_spring_frost, "firstFallFrost": first_fall_frost, "growingSeason": growing_season }
+    frost_data = { "location": loc, "altitude": alt, "lastSpringFrost": last_spring_frost, "firstFallFrost": first_fall_frost, "growingSeason": growing_season }
 
     render json: frost_data
   end
