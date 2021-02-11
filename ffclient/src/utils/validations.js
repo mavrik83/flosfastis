d51cd96433b6bf8798f38effa2f8eaf7d45b018b
mavrik83 <rjhuber83@gmail.com>
2021-02-10 21:34:10 -0500

    adjusted validations patterns

--- ffclient/src/utils/validations.js
+++ ffclient/src/utils/validations.js
@@ -4,12 +4,12 @@ export const required = (value) => {
 
 export const sowingFormat = (value) => {
   return value && !/^[0-9]{1,3}[-]{1}[0-9]{1,3} [a-z]{5} [a-z]{6}$/.test(value)
-    ? `Invalid format. Ex: "55-65 weeks before".`
+    ? `Invalid format. Ex: "4-6 weeks before".`
     : undefined;
 };
 
 export const daysToMatFormat = (value) => {
-  return value && !/^[0-9]{1,3}[-]{1}[0-9]{1,3}$/.test(value)
-    ? `Invalid format. Ex: "55-65"`
+  return value && !/^[0-9]{2}([-0-9]{0,2})?([0-9]{0,3})?$/.test(value)
+    ? `Invalid format. Ex: "55-65 -or- 90"`
     : undefined;
 };
